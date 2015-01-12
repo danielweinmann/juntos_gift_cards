a157ea26da1276df766d430da5ee970eecaaa70c
Daniel Weinmann <danielweinmann@gmail.com>
2015-01-12 19:37:28 -0200

    Adds API authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   after_action :verify_authorized, unless: -> {devise_controller?}
   after_action :verify_policy_scoped, only: %i[index], unless: -> {devise_controller?}
+  before_action :authenticate_api!
   before_action :authenticate_user!
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -26,6 +27,19 @@ class ApplicationController < ActionController::Base
     names = self.class.to_s.split('::')
     return "null" if names.length < 2
     names[0..(names.length-2)].map(&:downcase).join('_')
+  end
+
+  def authenticate_api!
+    if request.headers["HTTP_AUTHORIZATION"].present?
+      authenticate_or_request_with_http_token do |token, options|
+        api_key = ApiKey.not_expired.find_by_access_token(token)
+        if api_key
+          sign_in(:user, api_key.user)
+        else
+          raise Pundit::NotAuthorizedError
+        end
+      end
+    end
   end
 
 end
