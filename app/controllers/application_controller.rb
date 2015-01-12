77ae8bb37279dc2e21fff9a95d374ab9d92b9f81
Daniel Weinmann <danielweinmann@gmail.com>
2015-01-12 20:54:17 -0200

    Refactors API authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
 
+  before_action :authenticate_api!, if: -> { request.headers["HTTP_AUTHORIZATION"].present? }
+  skip_before_filter :verify_authenticity_token, if: -> { request.headers["HTTP_AUTHORIZATION"].present? }
   after_action :verify_authorized, unless: -> {devise_controller?}
   after_action :verify_policy_scoped, only: %i[index], unless: -> {devise_controller?}
-  before_action :authenticate_api!
   before_action :authenticate_user!
-  skip_before_filter :verify_authenticity_token, if: -> { request.headers["HTTP_AUTHORIZATION"].present? }
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -31,14 +31,12 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate_api!
-    if request.headers["HTTP_AUTHORIZATION"].present?
-      authenticate_or_request_with_http_token do |token, options|
-        api_key = ApiKey.not_expired.find_by_access_token(token)
-        if api_key
-          sign_in(:user, api_key.user)
-        else
-          return render json: { error: t('unauthorized') }, status: :unauthorized
-        end
+    authenticate_or_request_with_http_token do |token, options|
+      api_key = ApiKey.not_expired.find_by_access_token(token)
+      if api_key
+        sign_in(:user, api_key.user)
+      else
+        return render json: { error: t('unauthorized') }, status: :unauthorized
       end
     end
   end
